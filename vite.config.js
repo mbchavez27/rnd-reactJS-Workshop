136461d7432a61c5e80ac320d3ef02cf57ba6655
mbchavez27 <epikstrykermaxchavez23@gmail.com>
2025-06-24 18:33:29 +0800

    feat: setup reactjs

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,7 @@
 import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-export default defineConfig({ plugins: [tailwindcss()] })
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
